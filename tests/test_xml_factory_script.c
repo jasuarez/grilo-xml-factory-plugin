361066fb07d5eba952f34d479dde274db8cb8e90
Juan A. Suarez Romero <jasuarez@igalia.com>
2013-12-29 17:36:21 +0000

    Add full test for script

--- tests/test_xml_factory_script.c
+++ tests/test_xml_factory_script.c
@@ -57,6 +57,118 @@ test_xml_factory_script_init_fail (void)
   g_assert (!source);
 }
 
+static void
+test_xml_factory_script_return_string (void)
+{
+  GError *error = NULL;
+  GList *medias;
+  GrlMedia *media;
+  GrlOperationOptions *options;
+  GrlRegistry *registry;
+  GrlSource *source;
+
+  registry = grl_registry_get_default ();
+  source = grl_registry_lookup_source (registry, "xml-test-script-init-success");
+  g_assert (source);
+  options = grl_operation_options_new (NULL);
+  medias = grl_source_search_sync (source,
+                                   "string",
+                                   grl_source_supported_keys (source),
+                                   options,
+                                   &error);
+  g_assert_cmpint (g_list_length (medias), ==, 1);
+  g_assert_no_error (error);
+
+  media = (GrlMedia *) medias->data;
+
+  g_assert_cmpstr (grl_media_get_id (media),
+                   ==,
+                   "1");
+  g_assert_cmpstr (grl_media_audio_get_artist (GRL_MEDIA_AUDIO (media)),
+                   ==,
+                   "John Doe");
+  g_assert_cmpstr (grl_media_get_title (media),
+                   ==,
+                   "title foo");
+
+  g_list_free_full (medias, g_object_unref);
+  g_object_unref (options);
+}
+
+static void
+test_xml_factory_script_return_number (void)
+{
+  GError *error = NULL;
+  GList *medias;
+  GrlMedia *media;
+  GrlOperationOptions *options;
+  GrlRegistry *registry;
+  GrlSource *source;
+
+  registry = grl_registry_get_default ();
+  source = grl_registry_lookup_source (registry, "xml-test-script-init-success");
+  g_assert (source);
+  options = grl_operation_options_new (NULL);
+  medias = grl_source_search_sync (source,
+                                   "number",
+                                   grl_source_supported_keys (source),
+                                   options,
+                                   &error);
+  g_assert_cmpint (g_list_length (medias), ==, 1);
+  g_assert_no_error (error);
+
+  media = (GrlMedia *) medias->data;
+
+  g_assert_cmpstr (grl_media_get_id (media),
+                   ==,
+                   "1");
+  g_assert_cmpstr (grl_media_audio_get_artist (GRL_MEDIA_AUDIO (media)),
+                   ==,
+                   "John Doe");
+  g_assert_cmpstr (grl_media_get_title (media),
+                   ==,
+                   "42");
+
+  g_list_free_full (medias, g_object_unref);
+  g_object_unref (options);
+}
+
+static void
+test_xml_factory_script_return_invalid (void)
+{
+  GError *error = NULL;
+  GList *medias;
+  GrlMedia *media;
+  GrlOperationOptions *options;
+  GrlRegistry *registry;
+  GrlSource *source;
+
+  registry = grl_registry_get_default ();
+  source = grl_registry_lookup_source (registry, "xml-test-script-init-success");
+  g_assert (source);
+  options = grl_operation_options_new (NULL);
+  medias = grl_source_search_sync (source,
+                                   "invalid",
+                                   grl_source_supported_keys (source),
+                                   options,
+                                   &error);
+  g_assert_cmpint (g_list_length (medias), ==, 1);
+  g_assert_no_error (error);
+
+  media = (GrlMedia *) medias->data;
+
+  g_assert_cmpstr (grl_media_get_id (media),
+                   ==,
+                   "1");
+  g_assert_cmpstr (grl_media_audio_get_artist (GRL_MEDIA_AUDIO (media)),
+                   ==,
+                   "John Doe");
+  g_assert (!grl_media_get_title (media));
+
+  g_list_free_full (medias, g_object_unref);
+  g_object_unref (options);
+}
+
 int
 main(int argc, char **argv)
 {
@@ -75,6 +187,9 @@ main(int argc, char **argv)
 
   g_test_add_func ("/xml-factory/script/init-success", test_xml_factory_script_init_success);
   g_test_add_func ("/xml-factory/script/init-fail", test_xml_factory_script_init_fail);
+  g_test_add_func ("/xml-factory/script/return-string", test_xml_factory_script_return_string);
+  g_test_add_func ("/xml-factory/script/return-number", test_xml_factory_script_return_number);
+  g_test_add_func ("/xml-factory/script/return-invalid", test_xml_factory_script_return_invalid);
 
   return g_test_run ();
 }
